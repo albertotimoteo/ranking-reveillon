8191f783b67745564ca80bbda4467a1a48e21e14
Alberto Timoteo <alberto.pptimoteo@gmail.com>
2023-12-29 19:37:53 -0300

    feat: finished project

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Gold from "./assets/images/gold_medal.png"
 import Silver from "./assets/images/silver_medal.png"
 import Bronze from "./assets/images/bronze_medal.png"
 import "./assets/styles/App.css"
+import Winners from "./Winners"
 
 const getAsset = (index) => {
   if (index === 0) {
@@ -28,6 +29,7 @@ function App() {
   const [isShowingForm, setIsShowingForm] = useState(false)
   const [isEditing, setIsEditing] = useState(null)
   const [newScore, setNewScore] = useState(0)
+  const [screen, setScreen] = useState("RANKING")
 
   const orderArrayAndSave = (list) => {
     list.sort((a, b) => Number(b.score) - Number(a.score))
@@ -36,135 +38,146 @@ function App() {
 
   return (
     <div className="container">
-      <div className="titleContainer">
-        <img src={Beer} width={120} height={200} />
-        <div className="titleDiv">
-          <h1>
-            {!startDate
-              ? "Corrida ainda não iniciada"
-              : `Corrida iniciada: ${new Date(startDate).toLocaleDateString(
-                  "pt-BR"
-                )}`}
-          </h1>
-          <h1>
-            {!finishDate
-              ? "Corrida não finalizada"
-              : `Finalizada: ${new Date(finishDate).toLocaleDateString(
-                  "pt-BR"
-                )}`}
-          </h1>
-        </div>
-        <img src={Beer} width={120} height={200} />
-      </div>
-      <table className="table">
-        <tr>
-          <th>#</th>
-          <th>NOME:</th>
-          <th>TEOR ALCOÓLICO:</th>
-          {Boolean(startDate) && <th>Editar</th>}
-        </tr>
-        {JSON.parse(personsList).map((mapPerson, index) => (
-          <tr key={index}>
-            <td>{getAsset(index)}</td>
-            <td>{mapPerson.name}</td>
-            {isEditing === index ? (
-              <td>
-                <input
-                  value={newScore}
-                  onChange={(event) => setNewScore(event.target.valueAsNumber)}
-                  type="number"
-                />
-              </td>
-            ) : (
-              <td>{mapPerson.score}</td>
-            )}
-            {startDate && !finishDate && (
-              <button
-                onClick={() => {
-                  if (isEditing === null) {
-                    setNewScore(mapPerson.score)
-                    setIsEditing(index)
-                    return
-                  }
-                  orderArrayAndSave([
-                    ...JSON.parse(personsList).filter(
-                      (listPerson) => listPerson.name !== mapPerson.name
-                    ),
-                    { ...mapPerson, score: newScore },
-                  ])
-                  setIsEditing(null)
-                  setNewScore(null)
-                }}
-              >
-                {isEditing === index ? "OK" : <img src={Edit} />}
+      {screen === "RANKING" ? (
+        <>
+          <div className="titleContainer">
+            <img src={Beer} width={120} height={200} />
+            <div className="titleDiv">
+              <h1>
+                {!startDate
+                  ? "Corrida ainda não iniciada"
+                  : `Corrida iniciada: ${new Date(startDate).toLocaleDateString(
+                      "pt-BR"
+                    )}`}
+              </h1>
+              <h1>
+                {!finishDate
+                  ? "Corrida não finalizada"
+                  : `Finalizada: ${new Date(finishDate).toLocaleDateString(
+                      "pt-BR"
+                    )}`}
+              </h1>
+            </div>
+            <img src={Beer} width={120} height={200} />
+          </div>
+          <table className="table">
+            <tr>
+              <th>#</th>
+              <th>NOME:</th>
+              <th>TEOR ALCOÓLICO:</th>
+              {Boolean(startDate) && <th>Editar</th>}
+            </tr>
+            {JSON.parse(personsList).map((mapPerson, index) => (
+              <tr key={index}>
+                <td>{getAsset(index)}</td>
+                <td>{mapPerson.name}</td>
+                {isEditing === index ? (
+                  <td>
+                    <input
+                      value={newScore}
+                      onChange={(event) => setNewScore(event.target.value)}
+                      type="number"
+                    />
+                  </td>
+                ) : (
+                  <td>{mapPerson.score}</td>
+                )}
+                {startDate && !finishDate && (
+                  <button
+                    onClick={() => {
+                      if (isEditing === null) {
+                        setNewScore(mapPerson.score)
+                        setIsEditing(index)
+                        return
+                      }
+                      orderArrayAndSave([
+                        ...JSON.parse(personsList).filter(
+                          (listPerson) => listPerson.name !== mapPerson.name
+                        ),
+                        { ...mapPerson, score: newScore },
+                      ])
+                      setIsEditing(null)
+                      setNewScore(null)
+                    }}
+                  >
+                    {isEditing === index ? "OK" : <img src={Edit} />}
+                  </button>
+                )}
+              </tr>
+            ))}
+          </table>
+          <div>
+            <button onClick={() => setIsShowingForm(true)}>
+              Adicionar novo participante
+            </button>
+            <button onClick={() => setStartDate(new Date())}>
+              Iniciar corrida
+            </button>
+            <button
+              onClick={() => {
+                if (!finishDate) {
+                  setFinishDate(new Date())
+                  return
+                }
+                setFinishDate("")
+              }}
+            >
+              {!finishDate ? "Finalizar corrida" : "Reabrir corrida"}
+            </button>
+            {Boolean(finishDate) && (
+              <button onClick={() => setScreen("RESULT")}>
+                Mostrar vencedores
               </button>
             )}
-          </tr>
-        ))}
-      </table>
-      <div>
-        <button onClick={() => setIsShowingForm(true)}>
-          Adicionar novo participante
-        </button>
-        <button onClick={() => setStartDate(new Date())}>
-          Iniciar corrida
-        </button>
-        <button
-          onClick={() => {
-            if (!finishDate) {
-              setFinishDate(new Date())
-              return
-            }
-            setFinishDate("")
-          }}
-        >
-          {!finishDate ? "Finalizar corrida" : "Reabrir corrida"}
-        </button>
-        {Boolean(finishDate) && (
-          <button onClick={() => {}}>Mostrar vencedores</button>
-        )}
-        <button
-          onClick={() => {
-            setPersonsList("[]")
-            setFinishDate("")
-            setStartDate("")
-          }}
-        >
-          Limpar dados
-        </button>
-      </div>
-      {isShowingForm && (
-        <div>
-          <label>
-            Nome:
-            <input
-              value={person.name}
-              onChange={(event) =>
-                setPerson({ ...person, name: event.target.value })
-              }
-            />
-          </label>
-          <label>
-            Teor Alcoólico:
-            <input
-              type="number"
-              value={person.score}
-              onChange={(event) =>
-                setPerson({ ...person, score: event.target.value })
-              }
-            />
-          </label>
-          <button
-            onClick={() => {
-              orderArrayAndSave([...JSON.parse(personsList), { ...person }])
-            }}
-          >
-            Adicionar
-          </button>
-          <button onClick={() => setIsShowingForm(false)}>
-            Finalizar adição
-          </button>
-        </div>
+            <button
+              onClick={() => {
+                setPersonsList("[]")
+                setFinishDate("")
+                setStartDate("")
+              }}
+            >
+              Limpar dados
+            </button>
+          </div>
+          {isShowingForm && (
+            <div>
+              <label>
+                Nome:
+                <input
+                  value={person.name}
+                  onChange={(event) =>
+                    setPerson({ ...person, name: event.target.value })
+                  }
+                />
+              </label>
+              <label>
+                Teor Alcoólico:
+                <input
+                  type="number"
+                  value={person.score}
+                  onChange={(event) =>
+                    setPerson({ ...person, score: event.target.value })
+                  }
+                />
+              </label>
+              <button
+                onClick={() => {
+                  orderArrayAndSave([...JSON.parse(personsList), { ...person }])
+                }}
+              >
+                Adicionar
+              </button>
+              <button onClick={() => setIsShowingForm(false)}>
+                Finalizar adição
+              </button>
+            </div>
+          )}
+        </>
+      ) : (
+        <Winners
+          winners={JSON.parse(personsList).slice(0, 3)}
+          setScreen={setScreen}
+        />
       )}
     </div>
   )
--- src/Winners.jsx
+++ src/Winners.jsx
@@ -1,11 +1,43 @@
-const Winners = ({ winners }) => {
+/* eslint-disable react/prop-types */
+import Beer from "./assets/images/beers.png"
+import Gold from "./assets/images/gold_medal.png"
+import Silver from "./assets/images/silver_medal.png"
+import Bronze from "./assets/images/bronze_medal.png"
+import "./assets/styles/Winners.css"
+
+const Winners = ({ winners, setScreen }) => {
+  console.log(typeof winners[2].score)
   return (
-    <div>
-      <div>
+    <>
+      <div className="wrapper">
         <h3>Os maiores cachaceiros de</h3>
-        <h1>2023</h1>
+        <div className="yearContainer">
+          <img src={Beer} width={80} height={120} />
+          <h1>2023</h1>
+          <img src={Beer} width={80} height={120} />
+        </div>
       </div>
-    </div>
+      <div>
+        <div className="winner">
+          <img src={Gold} width={200} height={250} />
+          <span>{winners[0].name}</span>
+          <span>{Number(winners[0].score).toFixed(2)}</span>
+        </div>
+        <div className="secondRow">
+          <div className="winner">
+            <img src={Silver} width={200} height={250} />
+            <span>{winners[1].name}</span>
+            <span>{Number(winners[1].score).toFixed(2)}</span>
+          </div>
+          <div className="winner">
+            <img src={Bronze} width={200} height={250} />
+            <span>{winners[2].name}</span>
+            <span>{Number(winners[2].score).toFixed(2)}</span>
+          </div>
+        </div>
+        <button onClick={() => setScreen("RANKING")}>Voltar</button>
+      </div>
+    </>
   )
 }
 
